ac414eebcb5f48ec53b2623a40a2d838fd53774e
comfort-parker <comfortparker2001@outlook.com>
2025-09-14 15:37:26 +0000

    ec

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./auth.css";
+import "./Auth.css";
 import groceries from "../assets/groceries.gif";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
